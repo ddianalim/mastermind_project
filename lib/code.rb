f70097bac48adf54eb2279b89e071a142923db7c
ddianalim <dianalim1117@gmail.com>
2020-11-14 17:38:45 -0800

    pt 1

--- lib/code.rb
+++ lib/code.rb
@@ -6,4 +6,20 @@ class Code
     "Y" => :yellow
   }
 
+  def self.valid_pegs?(arr)
+    arr.each do |char|
+      if !POSSIBLE_PEGS.has_key?(char.upcase)
+        return false
+      end
+    end
+    return true
+  end
+
+  def initialize(arr)
+    if Code.valid_pegs?(arr)
+      @pegs = arr.upcase
+    else
+      raise "array does not contain valid pegs"
+    end
+  end
 end
