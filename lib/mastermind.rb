661314fef91af8db78c357787fb88d8686d27757
ddianalim <dianalim1117@gmail.com>
2020-11-15 16:23:00 -0800

    print_matches

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -4,4 +4,9 @@ class Mastermind
     def initialize(length)
         @secret_code = Code.random(length)
     end
+
+    def print_matches(code)
+        p @secret_code.num_exact_matches(code)
+        p @secret_code.num_near_matches(code)
+    end
 end
